19d9e58743081c30581ee9e80987ebb76943b9ff
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-18 20:33:37 +0300

    done Task 05 and 05var0

--- Task05var0/Program.cs
+++ Task05var0/Program.cs
@@ -5,7 +5,7 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 //  2 -> " -2, -1, 0, 1, 2"
 Console.Clear();
-Console.Write("Введите целое число1: ");
+Console.Write("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int count = -num; 
 while (count<=num) 
