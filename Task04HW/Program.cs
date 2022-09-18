9628315ba4371aaf6732c1a9a4054a232a530392
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-18 22:57:30 +0300

    done Task04HW

---
+++ Task04HW/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая 
+// принимает на вход три числа
+//  выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите цвторое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max=num1;
+if(num2>max ) max=num2;
+if(num3>max ) max=num3;
+Console.Write("max = ");
+Console.WriteLine(max);
