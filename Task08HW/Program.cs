6a8311ded77cbbcb4932cfba8dbaa0f8f8aa163c
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-19 02:03:07 +0300

    done Task08HW

---
+++ Task08HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая
+// на вход принимает число (N), 
+//выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <=0) Console.Write("Диапазон для поиска четных чисел не задан");
+else Console.WriteLine($"Вывожу четные числа от 1 до {num}:");
+int count = 2;
+while (count>=2 && count<=num) 
+{
+    if (count % 2 == 0)
+    {
+        
+        Console.WriteLine($"{count}");
+    }
+    count ++ ;
+}
